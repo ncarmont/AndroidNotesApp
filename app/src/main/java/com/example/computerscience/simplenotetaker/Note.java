9656c0d73f3492e37cbd2b5e323e4478033c298b
olcarmontzaragoza <olcarmontzaragoza@gmail.com>
2019-03-04 20:35:53 -0800

    initial commit

---
+++ app/src/main/java/com/example/computerscience/simplenotetaker/Note.java
@@ -0,0 +1,54 @@
+package com.example.computerscience.simplenotetaker;
+
+import android.content.Context;
+
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class Note implements Serializable {
+
+    private long mDateTime;
+    private String mTitle;
+    private String mContent;
+
+    public Note(long mDateTime, String mTitle, String mContent) {
+        this.mDateTime = mDateTime;
+        this.mTitle = mTitle;
+        this.mContent = mContent;
+    }
+
+    public void setmDateTime(long mDateTime) {
+        this.mDateTime = mDateTime;
+    }
+
+    public void setmTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    public void setmContent(String mContent) {
+        this.mContent = mContent;
+    }
+
+    public long getmDateTime() {
+        return mDateTime;
+    }
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public String getmContent() {
+        return mContent;
+     }
+
+     public String getDateTimeFormatted(Context context) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/mm/yyyy HH:mm:ss",
+                context.getResources().getConfiguration().locale);
+
+        sdf.setTimeZone(TimeZone.getDefault());
+        return sdf.format(new Date(mDateTime));
+     }
+
+}
---
+++ app/src/main/java/com/example/computerscience/simplenotetaker/NoteActivity.java
@@ -0,0 +1,54 @@
+package com.example.computerscience.simplenotetaker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class NoteActivity extends AppCompatActivity {
+
+    private EditText mEtTitle;
+    private EditText mEtContent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note);
+
+        mEtTitle = (EditText) findViewById(R.id.note_et_title);
+        mEtContent = (EditText) findViewById(R.id.note_et_content);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_note_new, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()){
+            case R.id.action_main_note_save:
+                saveNote();
+                break;
+        }
+        return true;
+    }
+
+    private void saveNote(){
+        Note note = new Note(System.currentTimeMillis(),mEtTitle.getText().toString(),
+                mEtContent.getText().toString());
+        if(Utilities.saveNote(this, note)){
+            Toast.makeText(this, "your note is saved", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "cannot save note, please ensure you have enough space", Toast.LENGTH_SHORT).show();
+        }
+        finish();
+
+    }
+
+
+}
